72d495e3967696790d77c9194a7ed00515b364fc
ndunas <ndunas@student.wsb-nlu.edu.pl>
2025-06-03 17:09:38 +0200

    [SNAKE-5 ] HOTFIX to Adding raylib

--- build-vs2022-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ndunas/source/repos/Snake/tests
-# Build directory: C:/Users/ndunas/source/repos/Snake/build-vs2022-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/ndunas/source/repos/Snake/build-vs2022-debug/tests/UnitTests-b12d07c_include.cmake")
-subdirs("../_deps/catch2-build")
