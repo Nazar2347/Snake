7f6f15eaedda4deb313001f97eeba1330c0bb121
Nazarii Dunas <dunas.nazar@gmail.com>
2025-06-18 16:37:59 +0200

    [SNAKE-10.1] Added Snake Class  - Added vector2 fields :     array of tail position     HeadPosition  - Added getters for those fields  - Added Constructor for Snake wich takes a starting position  - Linked RayLibLibrary to SnakeCoreLibrary  - Added Destructor

--- source/core/include/Snake.h
+++ source/core/include/Snake.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <raylib.h>
+#include <vector>
 
 class Snake
 {
-public:
 private:
+	std::vector<Vector2> TailPosition_;
+	Vector2 HeadPosition_;
+
+public:
+	Snake(Vector2 StartingPos);
+	std::vector<Vector2> getTailPosition();
+	Vector2 getHeadPosition();
+	virtual ~Snake();
+	
 };
--- source/core/src/Snake.cpp
+++ source/core/src/Snake.cpp
@@ -1,1 +1,22 @@
 #include "Snake.h"
+
+Snake::Snake(Vector2 StartingPos): HeadPosition_(StartingPos)
+{
+	
+}
+
+std::vector<Vector2> Snake::getTailPosition()
+{
+	return TailPosition_;
+}
+
+Vector2 Snake::getHeadPosition()
+{
+	return HeadPosition_
+}
+
+Snake::~Snake()
+{
+	
+}
+
