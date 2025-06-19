6f84902aed42abbbce98f080ebee4bffc269cfa9
Nazarii Dunas <dunas.nazar@gmail.com>
2025-06-19 16:39:35 +0200

    [SNAKE-10.4] Added UnitTest for Snake class -Connected RayLib libraries for UnitTest -Connected SnakeCore library for UnitTest

--- tests/TestSnake.cpp
+++ tests/TestSnake.cpp
@@ -1,6 +1,60 @@
 #include <catch2/catch_test_macros.hpp>
+#include <raylib.h>
+#include "Snake.h"
 
-TEST_CASE("test_Snake1", "[test1]")
-{
-	REQUIRE(1 + 1 == 2);
+// Helper for comparing Vector2
+
+TEST_CASE("Snake construction() initializes head and tail", "[Snake]") {
+    Vector2 start = { 5, 5 };
+    Snake snake(start);
+
+    REQUIRE(snake.getHeadPosition() == start);
+
+    auto tail = snake.getTailPosition();
+    REQUIRE(tail.size() == 1);
+    REQUIRE(tail[0] == Vector2{ 6, 5 });
+}
+
+TEST_CASE("ExtendTailBy() increases tail size", "[Snake]") {
+    Snake snake({ 0, 0 });
+    size_t initialSize = snake.getTailPosition().size();
+
+    snake.ExtendTailBy(3);
+    auto tail = snake.getTailPosition();
+    REQUIRE(tail.size() >= initialSize); // Only extends if size > 1 and < 100
+}
+
+TEST_CASE("ShortTailBy() decreases tail size and can kill snake", "[Snake]") {
+    Snake snake({ 0, 0 });
+    snake.ExtendTailBy(2);
+    size_t before = snake.getTailPosition().size();
+
+    snake.ShortTailBy(1);
+    REQUIRE(snake.getTailPosition().size() == before - 1);
+    REQUIRE(snake.isAlive());
+
+    // Remove all tail elements
+    snake.ShortTailBy(100);
+    REQUIRE_FALSE(snake.isAlive());
+}
+
+TEST_CASE("Move() updates head and tail positions", "[Snake]") {
+    Snake snake({ 1, 1 });
+    auto oldHead = snake.getHeadPosition();
+    auto oldTail = snake.getTailPosition();
+
+    Vector2 dir = { 1, 0 };
+    snake.Move(dir);
+
+    auto newHead = snake.getHeadPosition();
+    REQUIRE(newHead.x == oldHead.x + dir.x);
+    REQUIRE(newHead.y == oldHead.y + dir.y);
+    // Note: Tail movement logic may need to be checked for correctness
+}
+
+TEST_CASE("isAlive() returns correct status", "[Snake]") {
+    Snake snake({ 0, 0 });
+    REQUIRE(snake.isAlive());
+    snake.ShortTailBy(100);
+    REQUIRE_FALSE(snake.isAlive());
 }
