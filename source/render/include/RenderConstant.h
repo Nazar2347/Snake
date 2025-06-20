75271887a2c8340eedfee1a77c38b7103eb30c0d
Nazarii Dunas <dunas.nazar@gmail.com>
2025-06-20 17:04:15 +0200

    [SNAKE-15] Added new RenderConstants - SCALE - for  size scale - BLOCKS_SCALE - for scale of blocks

--- source/render/include/RenderConstant.h
+++ source/render/include/RenderConstant.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <raylib.h>
-
-constexpr int CELL_SIZE = 30; /*!< Cell size in pixels */ 
+constexpr float SCALE = 2.0f;
+constexpr float CELL_SIZE = 30*SCALE; /*!< Cell size in pixels */ 
 constexpr float BOARD_INITIAL_X_POS = 50.0f;
 constexpr float BOARD_INITIAL_Y_POS = 50.0f;
+constexpr float BLOCKS_SCALE = (SCALE / 10) + 0.01f;
