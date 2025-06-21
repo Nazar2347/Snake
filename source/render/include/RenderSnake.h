357239fc347e0b00dca1e031a20f5cdaba5cd9ba
Nazarii Dunas <dunas.nazar@gmail.com>
2025-06-21 12:40:40 +0200

    [SNAKE-12] Added RenderSnake class -Added EnumClass for snake parts location in trim sheet - Added Constructor - Added Draw() - Added DrawSnakePart() - Added destructor - Added snakeParts array to store parts of snake body in trim sheet

--- source/render/include/RenderSnake.h
+++ source/render/include/RenderSnake.h
@@ -1,2 +1,59 @@
 #pragma once
 #include <raylib.h>
+#include "RenderConstant.h"
+#include "Snake.h"
+
+
+
+enum SnakePartType {
+    BODY_TURN_TOP_RIGHT = 0,
+    BODY_HORIZONTAL,
+    BODY_TURN_TOP_LEFT,
+    HEAD_UP,
+    HEAD_RIGHT,
+
+    BODY_TURN_DOWN_LEFT,
+    BODY_VERTICAL,
+    HEAD_LEFT,
+    HEAD_DOWN,
+
+    BODY_TURN_DOWN_RIGHT,
+    TAIL_UP,
+    TAIL_RIGHT,
+
+    TAIL_LEFT,
+    TAIL_DOWN,
+};
+
+class RenderSnake
+{
+public:
+	RenderSnake(Snake& Snake);
+	void Draw()const;
+    void DrawSnakePart(Rectangle Area, Vector2 Position)const;
+    virtual ~RenderSnake();
+private:
+	Snake Snake_;
+	Texture2D SnakeBodyTexture_;
+    Rectangle SnakeParts[15] = {
+        {0,   0, 64, 64},   //Body turn (top-left)
+        {64,  0, 64, 64},   //Body horizontal (top-middle)
+        {128, 0, 64, 64},   //Body turn (top-left)
+        {192, 0, 64, 64},   //Head up
+        {256, 0, 64, 64},   //Head right
+
+        {0,   64, 64, 64},  // Body turn (down-left),  
+        {128, 64, 64, 64},  // Body vertical
+        {192, 64, 64, 64},  // Head left
+        {256, 64,  64, 64},  // Head Down
+
+        {128, 128, 64, 64}, // Body turn (down-right)
+        {192, 128, 64, 64}, // Tail up
+        {256, 128, 64, 64}, // Tail right
+
+        {192,  192, 64, 64}, // Tail left
+        {256, 192, 64, 64},  // Tail down
+        
+    };
+
+};
--- source/render/src/RenderSnake.cpp
+++ source/render/src/RenderSnake.cpp
@@ -1,1 +1,27 @@
 #include "RenderSnake.h"
+
+
+RenderSnake::RenderSnake(Snake& Snake): Snake_(Snake)
+{
+    
+    Image ImageSnake = LoadImage("Assets/Snake-Graphics.png");
+    SnakeBodyTexture_ = LoadTextureFromImage(ImageSnake);
+
+    UnloadImage(ImageSnake);
+
+}
+void RenderSnake::Draw()const
+{
+
+}
+void RenderSnake::DrawSnakePart(Rectangle Area, Vector2 Position)
+{
+    DrawTextureRec(SnakeBodyTexture_, Area, Position, WHITE);
+}
+
+RenderSnake::~RenderSnake()
+{
+    UnloadTexture(SnakeBodyTexture_);
+}
+
+
