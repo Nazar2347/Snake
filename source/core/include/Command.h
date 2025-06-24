8d688f9f3428576a5f27beab366cde06d083dec1
Nazarii Dunas <dunas.nazar@gmail.com>
2025-06-24 10:45:12 +0200

    [SNAKE-7] Added Comments and Doxy documentation for  - InputHandler  - Command

--- source/core/include/Command.h
+++ source/core/include/Command.h
@@ -1,37 +1,92 @@
 #pragma once
 #include "Snake.h"
 
+/**
+ * @brief Abstract base class for all command objects.
+ *
+ * The Command class defines the interface for executing commands
+ * that operate on a Snake actor. Derived classes implement specific
+ * actions by overriding the execute() method.
+ */
 class Command
 {
 public:
-	virtual void execute(Snake& SnakeActor) = 0;
-	virtual ~Command();
+    /**
+     * @brief Executes the command on the given Snake actor.
+     * @param SnakeActor Reference to the Snake object to operate on.
+     */
+    virtual void execute(Snake& SnakeActor) = 0;
+
+    /**
+     * @brief Virtual destructor for safe polymorphic deletion.
+     */
+    virtual ~Command();
 protected:
 };
 
-class MoveUPCommand :public Command
+/**
+ * @brief Command to move the Snake up.
+ */
+class MoveUPCommand : public Command
 {
 public:
-	void execute(Snake& SnakeActor) override;
+    /**
+     * @brief Executes the move up action on the Snake.
+     * @param SnakeActor Reference to the Snake object.
+     */
+    void execute(Snake& SnakeActor) override;
 };
+
+/**
+ * @brief Command to move the Snake down.
+ */
 class MoveDownCommand : public Command
 {
 public:
-	void execute(Snake& SnakeActor) override;
+    /**
+     * @brief Executes the move down action on the Snake.
+     * @param SnakeActor Reference to the Snake object.
+     */
+    void execute(Snake& SnakeActor) override;
 };
+
+/**
+ * @brief Command to move the Snake right.
+ */
 class MoveRightCommand : public Command
 {
 public:
-	void execute(Snake& SnakeActor) override;
-};
-class MoveLeftCommand :public Command
-{
-public:
-	void execute(Snake& SnakeActor) override;
+    /**
+     * @brief Executes the move right action on the Snake.
+     * @param SnakeActor Reference to the Snake object.
+     */
+    void execute(Snake& SnakeActor) override;
 };
 
+/**
+ * @brief Command to move the Snake left.
+ */
+class MoveLeftCommand : public Command
+{
+public:
+    /**
+     * @brief Executes the move left action on the Snake.
+     * @param SnakeActor Reference to the Snake object.
+     */
+    void execute(Snake& SnakeActor) override;
+};
+
+/**
+ * @brief Command that performs no action.
+ *
+ * Used as a default or null object when no input is detected.
+ */
 class NullCommand : public Command
 {
 public:
-	void execute(Snake & SnakeActor) override;
+    /**
+     * @brief Executes the null action (does nothing).
+     * @param SnakeActor Reference to the Snake object.
+     */
+    void execute(Snake & SnakeActor) override;
 };
--- source/core/src/InputHandler.cpp
+++ source/core/src/InputHandler.cpp
@@ -1,44 +1,47 @@
 #include "InputHandler.h"
 
-
+// Constructor: Initializes command objects for each input direction and a null command.
 InputHandler::InputHandler()
 {
-	 ButtonW_ = new MoveUPCommand();
-	 ButtonD_ = new MoveRightCommand();
-	 ButtonA_ = new MoveLeftCommand();
-	 ButtonS_ = new MoveDownCommand();
-	 NullCommand_ = new NullCommand();
-}
-Command* InputHandler::HandleInput()
-{
-	if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
-	{
-		return ButtonW_;
-	}
-	else if(IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
-	{
-		return ButtonD_;
-	}
-	else if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))
-	{
-		return ButtonA_;
-	}
-	else if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
-	{
-		return ButtonS_;
-	}
-	else
-	{
-		return NullCommand_;
-	}
+    ButtonW_ = new MoveUPCommand();      // 'W' or 'Up Arrow' key: Move up
+    ButtonD_ = new MoveRightCommand();   // 'D' or 'Right Arrow' key: Move right
+    ButtonA_ = new MoveLeftCommand();    // 'A' or 'Left Arrow' key: Move left
+    ButtonS_ = new MoveDownCommand();    // 'S' or 'Down Arrow' key: Move down
+    NullCommand_ = new NullCommand();    // No input: Do nothing
 }
 
+// Checks for key presses and returns the corresponding command object.
+Command* InputHandler::HandleInput()
+{
+    if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
+    {
+        return ButtonW_; // Move up
+    }
+    else if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
+    {
+        return ButtonD_; // Move right
+    }
+    else if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))
+    {
+        return ButtonA_; // Move left
+    }
+    else if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
+    {
+        return ButtonS_; // Move down
+    }
+    else
+    {
+        return NullCommand_; // No relevant key pressed
+    }
+}
+
+// Destructor: Cleans up allocated command objects.
 InputHandler::~InputHandler()
 {
-	delete ButtonW_;
-	delete ButtonD_;
-	delete ButtonA_;
-	delete ButtonS_;
-	delete NullCommand_;
+    delete ButtonW_;
+    delete ButtonD_;
+    delete ButtonA_;
+    delete ButtonS_;
+    delete NullCommand_;
 }
 
