5406859bcb041b441e858afff88f4877af521531
Nazarii Dunas <dunas.nazar@gmail.com>
2025-07-01 10:09:41 +0200

    [SNAKE-14] Added Game.h and .cpp for structurizing - Changed CMakeLists.txt for main.exe - Added EGameLevel - to indicate current level to proced - Added Game class   - Added Game::ProcessInput()   - Added Game::Update()   - Added Game::Render()   - Added IsGameOver() - to check if Snake is alive   - Added InitializeLevel1 /2/3() - for initialiszing all Game fields accoring to level

---
+++ source/app/Game/Game.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Board.h"
+#include "RenderBoard.h"
+#include "RenderFood.h"
+#include "RenderSnake.h"
+#include "InputHandler.h"
+#include <stack>
+
+enum class EGameLevel
+{
+	LEVEL1,
+	LEVEL2,
+	LEVEL3
+};
+
+class Game
+{
+public:
+	Game(EGameLevel);
+	void ProcessInput();
+	void Update();
+	void Render();
+	bool IsGameOver();
+	virtual ~Game();
+protected:
+	void InitializeLevel1();
+	void InitializeLevel2();
+	void InitializeLevel3();
+private:
+	Board* Level_;
+	Snake* PlayerSnake_;
+	RenderBoard* BoardRender_;
+	RenderSnake* SnakeRender_;
+	FoodRender* FoodRender_;
+	std::stack<IFood*> Level1FoodStack;
+	InputHandler InputHandler_;
+	Command* PlayerCommand_;
+};
