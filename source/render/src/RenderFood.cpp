009a3501947e8fe24715c3b259f80bbce3a5a90e
Nazarii Dunas <dunas.nazar@gmail.com>
2025-07-12 12:27:04 +0200

    [SNAKE-36] Clang Applied Fix on RenderFood

--- source/render/src/RenderFood.cpp
+++ source/render/src/RenderFood.cpp
@@ -22,31 +22,31 @@ void FoodRender::Draw(const IFood &Object)
     {
         // Draw apple texture at the food's position
         DrawTextureEx(AppleTexture_,
-                      {Object.GetPosition().x * UI::CELL_SIZE + UI::BOARD_INITIAL_X_POS,
-                       Object.GetPosition().y * UI::CELL_SIZE + UI::BOARD_INITIAL_Y_POS},
-                      0.0f, UI::OBJECTS_SCALE, WHITE);
+                      {(Object.GetPosition().x * UI::CELL_SIZE) + UI::BOARD_INITIAL_X_POS,
+                       (Object.GetPosition().y * UI::CELL_SIZE) + UI::BOARD_INITIAL_Y_POS},
+                      0.0F, UI::OBJECTS_SCALE, WHITE);
     }
     else if (Object.GetFoodType() == EFoodType::FROG)
     {
         // Draw frog texture at the food's position
         DrawTextureEx(FrogTexture_,
-                      {Object.GetPosition().x * UI::CELL_SIZE + UI::BOARD_INITIAL_X_POS,
-                       Object.GetPosition().y * UI::CELL_SIZE + UI::BOARD_INITIAL_Y_POS},
-                      0.0f, UI::OBJECTS_SCALE, WHITE);
+                      {(Object.GetPosition().x * UI::CELL_SIZE) + UI::BOARD_INITIAL_X_POS,
+                       (Object.GetPosition().y * UI::CELL_SIZE) + UI::BOARD_INITIAL_Y_POS},
+                      0.0F, UI::OBJECTS_SCALE, WHITE);
     }
     else if (Object.GetFoodType() == EFoodType::MOUSE)
     {
         // Draw mouse texture at the food's position
         DrawTextureEx(MouseTexture_,
-                      {Object.GetPosition().x * UI::CELL_SIZE + UI::BOARD_INITIAL_X_POS,
-                       Object.GetPosition().y * UI::CELL_SIZE + UI::BOARD_INITIAL_Y_POS},
-                      0.0f, UI::OBJECTS_SCALE, WHITE);
+                      {(Object.GetPosition().x * UI::CELL_SIZE) + UI::BOARD_INITIAL_X_POS,
+                       (Object.GetPosition().y * UI::CELL_SIZE) + UI::BOARD_INITIAL_Y_POS},
+                      0.0F, UI::OBJECTS_SCALE, WHITE);
     }
     else
     {
         // Draw a red rectangle for unknown food types
-        DrawRectangle((int)(Object.GetPosition().x * UI::CELL_SIZE + UI::BOARD_INITIAL_X_POS),
-                      (int)(Object.GetPosition().y * UI::CELL_SIZE + UI::BOARD_INITIAL_Y_POS), (int)UI::OBJECTS_SCALE,
+        DrawRectangle((int)((Object.GetPosition().x * UI::CELL_SIZE) + UI::BOARD_INITIAL_X_POS),
+                      (int)((Object.GetPosition().y * UI::CELL_SIZE) + UI::BOARD_INITIAL_Y_POS), (int)UI::OBJECTS_SCALE,
                       (int)UI::OBJECTS_SCALE, RED);
     }
 }
